b417631e7003c322c71619be82bf8a0184b8c532
Pat Whitrock <pwhitrock@constantcontact.com>
2015-05-20 22:41:38 -0400

    Add routing and controllers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,16 +1,27 @@
-require 'rulers/version'
 require 'rulers/array'
+require 'rulers/routing'
+require 'rulers/version'
 
 module Rulers
   class Application
     def call env
-      `echo debug > debug.txt`
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, { 'Content-Type' => 'text/html' }, []]
+      end
 
-      [
-        200,
-        { 'Content-Type' => 'text/html' },
-        ['Hello from Ruby on Rulers!']
-      ]
+      klass, act = get_controller_and_action env
+      controller = klass.new env
+      text = controller.send act
+
+      [200, { 'Content-Type' => 'text/html' }, [text]]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize env
+      @env = env
     end
   end
 end
